36cc0f260255222607978e5a4f60529d4ffee2fa
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-09-30 21:52:58 +0800

    Using App Engine SQL

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,31 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
 
-// Database Connection for Production
-
-// let config = {
-// 	user: process.env.SQL_USER,
-// 	database: process.env.SQL_DATABASE,
-// 	password: process.env.SQL_PASSWORD,
-// };
-//
-// if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-// 	config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-// }
-//
-// let connection = mysql.createConnection(config);
-
-
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	// Database Connection for Development
+	// Database Connection for Production
+    let config = {
+    	user: process.env.SQL_USER,
+    	database: process.env.SQL_DATABASE,
+    	password: process.env.SQL_PASSWORD,
+    };
 
-	let connection = mysql.createConnection({
-		host: process.env.DB_HOST,
-		user: process.env.DB_USER,
-		database: process.env.DB_DATABASE,
-		password: process.env.DB_PASS
-	});
+    if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+    	config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+    }
+
+    let connection = mysql.createConnection(config);
+
+
+	// Database Connection for Development
+	// let connection = mysql.createConnection({
+	// 	host: process.env.DB_HOST,
+	// 	user: process.env.DB_USER,
+	// 	database: process.env.DB_DATABASE,
+	// 	password: process.env.DB_PASS
+	// });
 
 	connection.connect(function(err) {
 		if (err) {
