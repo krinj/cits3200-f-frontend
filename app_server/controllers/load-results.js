2ef95387b3deafff6e258c500347355396358f0d
Yiran <yiranchen0828@gmail.com>
2018-10-21 21:24:02 +0800

    z

--- app_server/controllers/load-results.js
+++ app_server/controllers/load-results.js
@@ -2,20 +2,12 @@ var dbConnection = require('./db-connection');
 
 // LOAD/UPDATE THE PAGE WITH FILTERS SPECIFIED:
 module.exports.loadResults = function (req, res) {
-
+  
   var connection = dbConnection.connectToDB();
-
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("load-results.js has connected to database!");
-  });
-
-  // Organisation and survey details: ****TODO
-  // var organisation = req.body.organisation;
-  // var surveyID = req.body.surveyID;
-
+  
+  const projectid = 'cits-3200';
   // Filter variables, set to values sent to this controller from client:
-  var questionNum = req.body.questionNum;
+  var questionId = req.body.questionValue;
   var gender = req.body.gender;
   var ageRange = req.body.ageRange;
   var employStatus = req.body.employStatus;
@@ -24,8 +16,8 @@ module.exports.loadResults = function (req, res) {
 
   // Determine the start and end years of birth for different age ranges:
   var birthStart;
-  var birthEnd;
-  var currentYear = (new Date()).getFullYear();
+  var birthEnd; 
+  var currentYear = (new Date()).getFullYear(); 
   if (ageRange == 'all') {
     birthStart = 1900;   // No-one on Earth was born before then!
     birthEnd = currentYear;
@@ -33,7 +25,7 @@ module.exports.loadResults = function (req, res) {
     birthStart = currentYear - 18;
     birthEnd = currentYear;
   } else if (ageRange == 'age65plus') {
-    birthStart = 1900;
+    birthStart = 1900;  
     birthEnd = currentYear - 65;
   } else {
     var ageRangeSlice = ageRange.slice(3, 8);
@@ -41,119 +33,213 @@ module.exports.loadResults = function (req, res) {
     birthStart = currentYear - parseInt(ageRangeArray[1]);
     birthEnd = currentYear - parseInt(ageRangeArray[0]);
   }
+  
+  var query = [];
+  // Query 0: List of Questions for the Survey
+  query[0] = "SELECT distinct question_name,question_id FROM `cits-3200.analytics.responses_dev` R WHERE R.survey_id = 'd34f3eae8e9a48b6bc8c930bc5084b87';";
 
-  var queries = "";
+  // Query 1: Total response count:
+  query[1]= "SELECT COUNT(*) AS totalResponse FROM `cits-3200.analytics.responses_dev` R  WHERE R.employment_status = '"+ employStatus +"' AND R.survey_id ='d34f3eae8e9a48b6bc8c930bc5084b87' AND R.question_id = '"+ questionId +"' AND R.gender = '" + gender + "' AND R.timestamp BETWEEN '"+ startDate + "' AND '" + endDate +"' AND R.year_of_birth BETWEEN "+ birthStart + " AND "+ birthEnd +"; ";
 
-  // Query 0: Total response count:
-  queries += "SELECT COUNT(*) AS totalResponse FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.survey_id = 1 AND R.question_num = '" + questionNum + "' AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"'; ";
+  // Query 2: select all the responses
+  query[2]= "SELECT response FROM `cits-3200.analytics.responses_dev` R  WHERE R.employment_status = '" + employStatus + "' AND R.survey_id ='d34f3eae8e9a48b6bc8c930bc5084b87' AND R.question_id = '"+ questionId +"' AND R.gender = '" + gender + "' AND R.timestamp BETWEEN '"+ startDate + "' AND '" + endDate +"' AND R.year_of_birth BETWEEN "+ birthStart + " AND "+ birthEnd +"; ";
 
-  // Query 1: Number of completed responses for this question:
-  queries += "SELECT COUNT(*) AS completed FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '" + employStatus + "' AND R.survey_id = 1 AND R.question_num = '"+ questionNum +"' AND S.gender = '" + gender + "' AND R.char_count != 0 AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"'; ";
 
-  // Query 2: Average character count:
-  queries +="SELECT AVG(char_count) AS AverageCount FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
+  // Query 3: National average overall sentiment score for the question
+  query[3]= "SELECT AVG(overall_sentiment) AS overallAverage FROM `cits-3200.analytics.responses_dev` R  WHERE R.employment_status = '"+ employStatus +"' AND R.question_id = '"+ questionId +"' AND R.survey_id ='d34f3eae8e9a48b6bc8c930bc5084b87' AND R.gender = '" + gender + "' AND R.timestamp BETWEEN '"+ startDate + "' AND '" + endDate +"' AND R.year_of_birth BETWEEN "+ birthStart + " AND "+ birthEnd +" ;";
 
-  // Query 3: Max character count
-  queries += " SELECT MAX(char_count) AS maxCount FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
+  // Query 4: Organisation's average overall sentiment score for the question
+  query[4]= "SELECT AVG(overall_sentiment) AS organizationAverage FROM `cits-3200.analytics.responses_dev` R WHERE R.employment_status = '"+ employStatus +"' AND R.question_id = '"+ questionId +"' AND R.survey_id ='d34f3eae8e9a48b6bc8c930bc5084b87' AND R.gender = '" + gender + "' AND R.timestamp BETWEEN '"+ startDate + "' AND '" + endDate +"' AND R.year_of_birth BETWEEN "+ birthStart + " AND "+ birthEnd +" ;";
 
-  // Query 4: National average overall sentiment score for the question
-  queries += "SELECT AVG(overall_sentiment) AS overallAverage FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
+  // Query 5: First Date of responses for this question
+  query[5]= "SELECT timestamp AS firstDate FROM `cits-3200.analytics.responses_dev` R  WHERE R.survey_id ='d34f3eae8e9a48b6bc8c930bc5084b87' AND R.question_id = '" + questionId + "' ORDER BY firstDate ASC LIMIT 1;";
 
-  // Query 5: Organisation's average overall sentiment score for the question
-  queries += "SELECT AVG(overall_sentiment) AS organizationAverage FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
-
+  
   // Query 6: Frequency count array of sentiment scores -10 to 10 (for Histogram by Score)
-  queries += "SELECT overall_sentiment, count(*) as frequency FROM Response R, Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.submission_id = S.submission_id AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND R.char_count != 0 AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY overall_sentiment;";
+  query[6] = "SELECT overall_sentiment, count(*) as frequency FROM `cits-3200.analytics.responses_dev` R  WHERE R.employment_status = '"+ employStatus +"' AND R.survey_id ='d34f3eae8e9a48b6bc8c930bc5084b87' AND R.question_id = '"+ questionId +"' AND R.gender = '" + gender + "' AND R.timestamp BETWEEN '"+ startDate + "' AND '" + endDate +"' AND R.year_of_birth BETWEEN "+ birthStart + " AND "+ birthEnd +" GROUP BY overall_sentiment;";
 
-  // Query 7: Get overall sentiment time-series data:
-  queries += "SELECT date_submitted as ds, AVG(overall_sentiment) as avgOs FROM Submission S, Response R WHERE S.employment_status = '"+ employStatus +"' AND R.submission_id = S.submission_id AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND R.char_count != 0 AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"'  Group BY date_submitted order by date_submitted;";  
-
-  // Query 8: Get list of entities for entity search function
-  queries += "SELECT DISTINCT E.entity as ent FROM Submission S NATURAL JOIN Response R NATURAL JOIN Entity E WHERE E.entity IS NOT NULL AND question_num = " + questionNum + " AND S.gender = '" + gender + "' AND S.employment_status = '" + employStatus + "' AND S.date_submitted BETWEEN '" + startDate + "' AND '" + endDate + "' AND S.year_of_birth BETWEEN '" + birthStart + "' AND '" + birthEnd + "' ORDER BY E.entity;";
-
+  
   // Remove filters set to 'all' if applicable:
-  if (gender == 'all') {
-    queries = queries.replace(/S.gender = 'all' AND/g, '');
+  
+  var queryCopy = [];
+  for (var i= 0;i<query.length;i++){
+    queryCopy[i] = query[i];
+    if (gender == 'all') {
+      queryCopy[i] = queryCopy[i].replace(/R.gender = 'all' AND/, '');
+    }
+    if (employStatus == 'all') {
+      queryCopy[i] = queryCopy[i].replace(/R.employment_status = 'all' AND/, '');
+    }
+    if(questionId == 'all'){
+      queryCopy[i] =queryCopy[i].replace(/AND R.question_id = 'all'/,'');
+    }
   }
-  if (employStatus == 'all') {
-    queries = queries.replace(/S.employment_status = 'all' AND/g, '');
-  }
+  
+  
 
   // Run the SQL queries:
-  connection.query(queries, function (err, rows, fields) {
-    if (err) throw err;
-
+  
     // NB using underscores to distinguish from variables of same name sent to pug file
-    var num_responses;
-    var num_completed;
-    var percent_completed;
-    var ave_char_count;
-    var max_char_count;
-    var national_ave;
-    var organisation_ave;
-    var score_freq_array = [];
-    var time_series;
-    var entities = [];
+  var num_responses;
+  var num_completed=0;
+  var percent_completed;
+  var ave_char_count;
+  var max_char_count;
+  var national_ave;
+  var organization_ave;
+  var first_date;
+  var question_array = [];
+  var score_freq_array = [];
 
-    if (rows[1][0].completed == 0) { // this prevents errors from query results being null      
-      num_responses = 0;
-      num_completed = 0;
-      percent_completed = 0;
-      ave_char_count = 0;
-      max_char_count = 0;
-      national_ave = 0;
-      organisation_ave = 0;
-      for (i = 0; i < 21; i++) {
-        score_freq_array[i] = 0;
+ 
+  for (var i = 0; i<queryCopy.length;i++){
+    asyncQuery(queryCopy[i], projectid,i);
+    
+  }
+    
+    async function asyncQuery(sqlquery, projectid,queryIndex) {
+      // Imports the Google Cloud client library
+      const BigQuery = require('@google-cloud/bigquery');
+      const bigquery = new BigQuery({
+        projectId: projectid,
+      });  
+      projectId = projectid;
+        sqlQuery = sqlquery;
+      const options = {
+        query: sqlQuery,
+        useLegacySql: false, // Use standard SQL syntax for queries.
+        
+      };
+      
+      // Runs the query as a job
+      const [job] = await bigquery.createQueryJob(options);
+      console.log(`Job ${job.id} started.`);
+    
+      // Get the job's status
+      const metadata = await job.getMetadata();
+    
+      // Check the job's status for errors
+      const errors = metadata[0].status.errors;
+      if (errors && errors.length > 0) {
+        throw errors;
       }
-      time_series = [];
-    } 
-    else {
-      num_responses = rows[0][0].totalResponse;
-      num_completed = rows[1][0].completed;
-      percent_completed = parseInt((num_completed / num_responses) * 100);
-      ave_char_count = parseInt(rows[2][0].AverageCount);
-      max_char_count = rows[3][0].maxCount;
-      national_ave = rows[4][0].overallAverage;
-      organisation_ave = rows[5][0].organizationAverage;
-      var index = 0;
-      var arrayIndex = 0;
-      var responses = rows[6];
-      for (i = -10; i < 11; i++) {
-        if (i != responses[index].overall_sentiment) {
-          score_freq_array[arrayIndex] = 0;
-          arrayIndex++;
-        } else {
-          score_freq_array[arrayIndex] = responses[index].frequency;
-
-          index++;
-          if (index == responses.length) {
-            index--;
-          }
-          arrayIndex++;
+      console.log('Job ${job.id} completed.')
+    
+      const [rows] = await job.getQueryResults();
+      console.log('Rows:');
+      
+      if(queryIndex==0){
+        for (i = 0; i < rows.length; i++) {
+          question_array.push(rows[i]);
         }
+        console.log(question_array);
+      }else if (queryIndex ==1){
+        num_responses = rows[0].totalResponse;
+        console.log(num_responses);
+      }else if(queryIndex ==2){
+        var totalchar = 0;
+        max_char_count=0;
+        if(num_responses!=0){
+        for(var i = 0;i < num_responses;i++){
+          if(rows[i].response.length!=0){
+            num_completed++;
+          }
+          totalchar += rows[i].response.length;
+          
+          if(rows[i].response.length > max_char_count){
+            max_char_count = rows[i].response.length;
+          }
+        }
+        console.log(num_completed);
+        percent_completed = parseInt((num_completed / num_responses) * 100);
+        ave_char_count = parseInt(totalchar/num_responses);
+        console.log("Percent_completed"+percent_completed);
+        console.log("average char count"+ ave_char_count);
+      }else{
+        max_char_count = 0;
+        ave_char_count = 0;
       }
-      time_series = rows[7];
-      for (i = 0; i < rows[8].length; i++) {
-        entities.push(rows[8][i].ent);
+        
+      }else if (queryIndex ==3){
+        if(rows==[]){
+          national_ave =0;
+        }else{
+        national_ave = rows[0].overallAverage*10;
+        console.log("national ave" + national_ave);
+        }
+      }else if(queryIndex==4){
+        if(rows==[]){
+          organization_ave =0;
+        }else{
+        organization_ave = parseInt(rows[0].organizationAverage*10);
+        }
+      }else if (queryIndex == 5){
+        first_date =rows[0].first_Date.value;
+        console.log(first_date);
+      }else if (queryIndex == 6){
+        console.log(rows);
+        
+        var index = 0;
+        var arrayIndex = 0;
+         
+        for (i = -10; i < 11; i++) {
+         if (i != parseInt(rows[index].overall_sentiment*10)) {
+           score_freq_array[arrayIndex] = 0;
+           arrayIndex++;
+          } else {
+            score_freq_array[arrayIndex] = parseInt(rows[index].overall_sentiment*10)
+  
+           index++;
+           if (index == rows.length) {
+              index--;
+            }
+           arrayIndex++;
+         }
+       }
+       /*for(var j = 0;j<score_freq_array.length;j++){
+         console.log(score_freq_array[j]);
+       }*/
       }
+    
+      // [END bigquery_query]
     }
 
-    var results = {
-      numResponses: num_responses,
-      percentCompleted: percent_completed,
-      aveCharCount: ave_char_count,
-      maxCharCount: max_char_count,
-      orgAveSentiment: organisation_ave,
-      nationalAveSentiment: national_ave,
-      scoreFreqArray: score_freq_array,
-      timeSeries: time_series,
-      entityList: entities
-    };
+    setInterval(function(){
+      if((question_array && first_date && num_responses && percent_completed && ave_char_count && max_char_count && organization_ave && national_ave && score_freq_array) ){
+        var results = { 
+          questionArray: question_array,
+          
+         
+          firstDate: first_date.slice(0,10),
+          numResponses: num_responses,
+          percentCompleted: percent_completed,
+          aveCharCount: ave_char_count,
+          maxCharCount: max_char_count,
+          orgAveSentiment: organization_ave,
+          nationalAveSentiment: national_ave,
+          scoreFreqArray: score_freq_array,
+          // responseDetail: response_detail,
+          //responseDate: response_date,
+          //responseScore: response_score,
+          //responseText: response_text,
+        };
+        for(attrib in results) {
+          console.log("Attrib: " + attrib + " value: " + results[attrib]);
+        }
+        
+        return res.send(results);
+      }
+    }, 1000);
 
-    connection.end();
+    
 
-    return res.send(results);
-  });
 
+
+  
+  
+  
+  
+  
 };
+
+
