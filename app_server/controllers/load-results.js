7ac879751c333e6505b9e1dd44eebabe0f95d05e
Yiran <yiranchen0828@gmail.com>
2018-10-22 17:17:02 +0800

    z

--- app_server/controllers/load-results.js
+++ app_server/controllers/load-results.js
@@ -46,11 +46,11 @@ module.exports.loadResults = function (req, res) {
   
 
   // Query 2: select all the responses
-  query[0]= "SELECT count(*) FROM `cits-3200.analytics.responses_dev` R  WHERE R.employment_status = '" + employStatus + "' AND R.abn_hash = 'a11e075a60a41650aa6b8dad77fdd347aacb5e3ee850708c68de607f454f07d1' AND R.question_id = '"+ questionId +"' AND R.gender = '" + gender + "' AND R.timestamp BETWEEN '"+ startDate + "' AND '" + endDate +"' AND R.year_of_birth BETWEEN "+ birthStart + " AND "+ birthEnd +"; ";
-
+  query[0]= "SELECT count(*) as totalResponse FROM `cits-3200.analytics.responses_dev` R  WHERE R.employment_status = '" + employStatus + "' AND R.abn_hash = 'a11e075a60a41650aa6b8dad77fdd347aacb5e3ee850708c68de607f454f07d1' AND R.question_id = '"+ questionId +"' AND R.gender = '" + gender + "' AND R.timestamp BETWEEN '"+ startDate + "' AND '" + endDate +"' AND R.year_of_birth BETWEEN "+ birthStart + " AND "+ birthEnd +"; ";
+  
 
   // Query 3: National average overall sentiment score for the question
-  query[1]= "SELECT AVG(overall_sentiment) AS overallAverage FROM `cits-3200.analytics.responses_dev` R  WHERE R.employment_status = '"+ employStatus +"' AND R.question_id = '"+ questionId +"' AND R.abn_hash = 'a11e075a60a41650aa6b8dad77fdd347aacb5e3ee850708c68de607f454f07d1' AND R.gender = '" + gender + "' AND R.timestamp BETWEEN '"+ startDate + "' AND '" + endDate +"' AND R.year_of_birth BETWEEN "+ birthStart + " AND "+ birthEnd +" ;";
+  query[1]= "SELECT AVG(overall_sentiment) AS nationalAverage FROM `cits-3200.analytics.responses_dev` R  WHERE R.employment_status = '"+ employStatus +"' AND R.question_id = '"+ questionId +"' AND R.gender = '" + gender + "' AND R.timestamp BETWEEN '"+ startDate + "' AND '" + endDate +"' AND R.year_of_birth BETWEEN "+ birthStart + " AND "+ birthEnd +" ;";
 
   // Query 4: Organisation's average overall sentiment score for the question
   query[2]= "SELECT AVG(overall_sentiment) AS organizationAverage FROM `cits-3200.analytics.responses_dev` R WHERE R.employment_status = '"+ employStatus +"' AND R.question_id = '"+ questionId +"' AND R.abn_hash = 'a11e075a60a41650aa6b8dad77fdd347aacb5e3ee850708c68de607f454f07d1' AND R.gender = '" + gender + "' AND R.timestamp BETWEEN '"+ startDate + "' AND '" + endDate +"' AND R.year_of_birth BETWEEN "+ birthStart + " AND "+ birthEnd +" ;";
@@ -64,35 +64,35 @@ module.exports.loadResults = function (req, res) {
 
   
   // Remove filters set to 'all' if applicable:
-  var queries2 = "";
-// Query 0: Total response count:
-queries2 += "SELECT COUNT(*) AS totalResponse FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.survey_id = 1 AND R.question_num = '" + questionNum + "' AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"'; ";
+  /*var queries2 = "";
+  // Query 0: Total response count:
+  queries2 += "SELECT COUNT(*) AS totalResponse FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.survey_id = 1 AND R.question_num = '" + questionNum + "' AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"'; ";
 
-// Query 1: Number of completed responses for this question:
-queries2 += "SELECT COUNT(*) AS completed FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '" + employStatus + "' AND R.survey_id = 1 AND R.question_num = '"+ questionNum +"' AND S.gender = '" + gender + "' AND R.char_count != 0 AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"'; ";
+  // Query 1: Number of completed responses for this question:
+  queries2 += "SELECT COUNT(*) AS completed FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '" + employStatus + "' AND R.survey_id = 1 AND R.question_num = '"+ questionNum +"' AND S.gender = '" + gender + "' AND R.char_count != 0 AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"'; ";
 
-// Query 2: Average character count:
-queries2 +="SELECT AVG(char_count) AS AverageCount FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
+  // Query 2: Average character count:
+  queries2 +="SELECT AVG(char_count) AS AverageCount FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
 
-// Query 3: Max character count
-queries2 += " SELECT MAX(char_count) AS maxCount FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
+  // Query 3: Max character count
+  queries2 += " SELECT MAX(char_count) AS maxCount FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
 
-// Query 4: National average overall sentiment score for the question
-queries2 += "SELECT AVG(overall_sentiment) AS overallAverage FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
+  // Query 4: National average overall sentiment score for the question
+  queries2 += "SELECT AVG(overall_sentiment) AS overallAverage FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
 
-// Query 5: Organisation's average overall sentiment score for the question
-queries2 += "SELECT AVG(overall_sentiment) AS organizationAverage FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
+  // Query 5: Organisation's average overall sentiment score for the question
+  queries2 += "SELECT AVG(overall_sentiment) AS organizationAverage FROM Response R NATURAL JOIN Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY question_num;";
 
-// Query 6: Frequency count array of sentiment scores -10 to 10 (for Histogram by Score)
-queries2 += "SELECT overall_sentiment, count(*) as frequency FROM Response R, Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.submission_id = S.submission_id AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND R.char_count != 0 AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY overall_sentiment;";
+  // Query 6: Frequency count array of sentiment scores -10 to 10 (for Histogram by Score)
+  queries2 += "SELECT overall_sentiment, count(*) as frequency FROM Response R, Submission S WHERE S.employment_status = '"+ employStatus +"' AND R.submission_id = S.submission_id AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND R.char_count != 0 AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"' GROUP BY overall_sentiment;";
 
-// Query 7: Get overall sentiment time-series data:
-queries2 += "SELECT date_submitted as ds, AVG(overall_sentiment) as avgOs FROM Submission S, Response R WHERE S.employment_status = '"+ employStatus +"' AND R.submission_id = S.submission_id AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND R.char_count != 0 AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"'  Group BY date_submitted order by date_submitted;";  
+  // Query 7: Get overall sentiment time-series data:
+  queries2 += "SELECT date_submitted as ds, AVG(overall_sentiment) as avgOs FROM Submission S, Response R WHERE S.employment_status = '"+ employStatus +"' AND R.submission_id = S.submission_id AND R.question_num = '"+ questionNum +"' AND R.survey_id = 1 AND S.gender = '" + gender + "' AND R.char_count != 0 AND S.date_submitted BETWEEN '"+ startDate + "' AND '" + endDate +"' AND S.year_of_birth BETWEEN '"+ birthStart + "' AND '"+ birthEnd +"'  Group BY date_submitted order by date_submitted;";  
 
-// Query 8: Get list of entities for entity search function
-queries2 += "SELECT DISTINCT E.entity as ent FROM Submission S NATURAL JOIN Response R NATURAL JOIN Entity E WHERE E.entity IS NOT NULL AND question_num = " + questionNum + " AND S.gender = '" + gender + "' AND S.employment_status = '" + employStatus + "' AND S.date_submitted BETWEEN '" + startDate + "' AND '" + endDate + "' AND S.year_of_birth BETWEEN '" + birthStart + "' AND '" + birthEnd + "' ORDER BY E.entity;";
+  // Query 8: Get list of entities for entity search function
+  queries2 += "SELECT DISTINCT E.entity as ent FROM Submission S NATURAL JOIN Response R NATURAL JOIN Entity E WHERE E.entity IS NOT NULL AND question_num = " + questionNum + " AND S.gender = '" + gender + "' AND S.employment_status = '" + employStatus + "' AND S.date_submitted BETWEEN '" + startDate + "' AND '" + endDate + "' AND S.year_of_birth BETWEEN '" + birthStart + "' AND '" + birthEnd + "' ORDER BY E.entity;";
 
-
+  */
 
 
 
@@ -129,14 +129,14 @@ queries2 += "SELECT DISTINCT E.entity as ent FROM Submission S NATURAL JOIN Resp
   
     // NB using underscores to distinguish from variables of same name sent to pug file
   var num_responses;
-  var num_completed=0;
-  var percent_completed;
-  var ave_char_count;
-  var max_char_count;
+  
+  var percent_completed = 0;
+  var ave_char_count = 0;
+  var max_char_count= 0;
   var national_ave;
   var organization_ave;
   var first_date;
-  var question_array = [];
+  
   var score_freq_array = [];
 
  
@@ -159,6 +159,9 @@ queries2 += "SELECT DISTINCT E.entity as ent FROM Submission S NATURAL JOIN Resp
         
       };
       
+      if(queryIndex == 1){
+        console.log(sqlquery);
+      }
       // Runs the query as a job
       const [job] = await bigquery.createQueryJob(options);
       console.log(`Job ${job.id} started.`);
@@ -175,20 +178,22 @@ queries2 += "SELECT DISTINCT E.entity as ent FROM Submission S NATURAL JOIN Resp
     
       const [rows] = await job.getQueryResults();
       console.log('Rows:');
-      
+      console.log(rows);
       if(queryIndex==0){
+        num_responses = rows[0].totalResponse;
         console.log("response num is :" + rows[0].totalResponse);
       }
 
       else if (queryIndex ==1){
-        num_responses = rows[0].totalResponse;
-        console.log(num_responses);
+       
+        national_ave = rows[0].nationalAverage;
+        console.log("average national score is " + national_ave);
+      
       }
       
       else if(queryIndex ==2){
-        
-       
-        
+        organization_ave = rows[0].organizationAverage;
+        console.log("organization average score is sdklfjlkdsjflkdsj " + organization_ave);
         
         
       
@@ -235,50 +240,30 @@ queries2 += "SELECT DISTINCT E.entity as ent FROM Submission S NATURAL JOIN Resp
            arrayIndex++;
          }
        }
-       /*for(var j = 0;j<score_freq_array.length;j++){
-         console.log(score_freq_array[j]);
-       }*/
+       
       }
     
-      // [END bigquery_query]
+      
     }
 
-    setInterval(function(){
-      if((question_array && first_date && num_responses && percent_completed && ave_char_count && max_char_count && organization_ave && national_ave && score_freq_array) ){
-        var results = { 
-          questionArray: question_array,
-          
-         
-          firstDate: first_date.slice(0,10),
+    
+    var results;
+    var interval = setInterval(function() {
+      if((num_responses && percent_completed && ave_char_count&&max_char_count&& organization_ave && national_ave && score_freq_array && time_series && entities) ){
+        
+       
+          results = {
           numResponses: num_responses,
+          
           percentCompleted: percent_completed,
           aveCharCount: ave_char_count,
           maxCharCount: max_char_count,
-          orgAveSentiment: organization_ave,
+          orgAveSentiment: organisation_ave,
           nationalAveSentiment: national_ave,
           scoreFreqArray: score_freq_array,
-          // responseDetail: response_detail,
-          //responseDate: response_date,
-          //responseScore: response_score,
-          //responseText: response_text,
+          timeSeries: time_series,
+          entityList: entities
         };
-        for(attrib in results) {
-          console.log("Attrib: " + attrib + " value: " + results[attrib]);
-        }
-        connection.end();
-        return res.send(results);
-      }
-    }, 1000);
-    var results;
-    var interval = setInterval(function() {
-      if((question_array != null && first_date && last_date) ){
-        
-       
-        results = {
-          firstDate: first_date,
-          lastDate: last_date,
-          questionArray: question_array,
-        }; 
         
         clearInterval(interval);
         return res.send(results);
